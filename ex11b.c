11a2f356eb8daa6523cd3c5497890c3552025aa5
Paulo Vitor Alves Patriota <paulovitorpatriota@gmail.com>
2015-11-24 22:21:06 -0300

    Iniciando as declarações das variáveis para o programa

--- ex11b.c
+++ ex11b.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/shm.h>
+#include <signal.h> /*bibliotecas*/
+#include <string.h>
 
-int main ()
+#define tamsenha 5 /*tamanho da senha*/
+
+static volatile int controle; /*variavel pra controle da funcao dormir*/
+
+typedef struct jogoant /*struct para auxiliar a IA*/
 {
-    return EXIT_SUCCESS;
-}
+    char ant[tamsenha];
+    char resultant[tamsenha];
+    int nb, np, a, b, jogocerteza[tamsenha] = {0}, jogada;
+}jogoant;
+
+
